70bac1e6795c1eafe339ce61383038bedbc25de0
Bexxper <haikalfathir8@gmail.com>
2025-03-24 18:21:26 +0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,18 +1,30 @@
 function loadWhitelist()
-    local url = "https://raw.githubusercontent.com/Bexxper/CPS-Whitelist/refs/heads/main/Whitelist.txt" -- Ganti dengan link whitelist-mu
+    local url = "https://raw.githubusercontent.com/Bexxper/CPS-Whitelist/refs/heads/main/Whitelist.txt"
     local response = httprequest(url, "get")
     local whitelist = {}
 
     if response then
         for uid in response:gmatch("[^\r\n]+") do
-            whitelist[tonumber(uid)] = true
+            local numUID = tonumber(uid)
+            if numUID then -- Check if conversion to number was successful
+                whitelist[numUID] = true
+            else
+                log("Warning: Invalid UID in whitelist: " .. uid)
+            end
         end
+    else
+        log("Error: Failed to load whitelist from " .. url)
     end
     return whitelist
 end
 
--- Load daftar UID yang diizinkan
+-- Load the whitelist.  Error handling is included.
 whitelist = loadWhitelist()
+if not whitelist then
+    log("Whitelist failed to load. No players will be allowed.")
+    --Consider adding a fallback action, such as shutting down the server.
+    --os.exit() -- this will stop the script.  Use cautiously.
+end
 
 function isUIDAllowed(uid)
     return whitelist[uid] == true
